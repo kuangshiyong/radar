18f1bb5774b3885be0b09d1a5d1e0904aee6a82a
Selene <yashizheng@outlook.com>
2018-04-30 12:36:29 +0800

    edit annotation of application.java

--- src/main/java/cc/intelli/radar/RadarApplication.java
+++ src/main/java/cc/intelli/radar/RadarApplication.java
@@ -1,8 +1,19 @@
 package cc.intelli.radar;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+@EnableAutoConfiguration
+@EnableTransactionManagement
+@ServletComponentScan
+@ComponentScan
+@EnableScheduling
 @SpringBootApplication
 public class RadarApplication {
 
