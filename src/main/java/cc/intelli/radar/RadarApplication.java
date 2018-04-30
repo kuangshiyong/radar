56c38efd3a6d4442dd7db70a71f9f5ece976082f
wangsiyuan <wangsiyuancalvin_i@didichuxing.com>
2018-04-30 14:08:31 +0800

    Add startup steps

--- src/main/java/cc/intelli/radar/RadarApplication.java
+++ src/main/java/cc/intelli/radar/RadarApplication.java
@@ -1,12 +1,29 @@
 package cc.intelli.radar;
 
+import cc.intelli.radar.service.RadarServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class RadarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RadarApplication.class, args);
-	}
+    private final
+    RadarServiceImpl radarService;
+
+    @Autowired
+    public RadarApplication(RadarServiceImpl radarService) {
+        this.radarService = radarService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadarApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        radarService.importData();
+    }
 }
