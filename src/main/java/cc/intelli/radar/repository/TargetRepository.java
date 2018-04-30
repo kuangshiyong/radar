575447e6e2f857a5cff1913e36c61b843d225172
Selene <yashizheng@outlook.com>
2018-04-30 20:17:59 +0800

    Debug

--- src/main/java/cc/intelli/radar/repository/TargetRepository.java
+++ src/main/java/cc/intelli/radar/repository/TargetRepository.java
@@ -1,12 +1,12 @@
-package cc.intelli.radar.repository;
-
-import cc.intelli.radar.entity.Target;
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by Selene on 2018/4/30.
- *
- */
-public interface TargetRepository extends CrudRepository<Target, Long> {
-
-}
+//package cc.intelli.radar.repository;
+//
+//import cc.intelli.radar.entity.Target;
+//import org.springframework.data.repository.CrudRepository;
+//
+///**
+// * Created by Selene on 2018/4/30.
+// *
+// */
+//public interface TargetRepository extends CrudRepository<Target, Long> {
+//
+//}
